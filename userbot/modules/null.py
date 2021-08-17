1f20626ebed9d0fac798586c38e0eab19c882095
alzxd <88322724+alzxd@users.noreply.github.com>
2021-08-17 17:16:54 +0700

    Update null.py

--- userbot/modules/null.py
+++ userbot/modules/null.py
@@ -5,11 +5,11 @@ from time import sleep
 @register(outgoing=True, pattern='^.Ali(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(3)
+    sleep(2)
     await typew.edit("`Hai Perkenalkan Namaku ali`")
-    sleep(3)
+    sleep(2)
     await typew.edit("`22 Tahun`")
-    sleep(1)
+    sleep(2)
     await typew.edit("`Tinggal Di Cibubur, Salam Kenal:)`")
 # Create by myself @localheart
 
@@ -17,23 +17,23 @@ async def typewriter(typew):
 @register(outgoing=True, pattern='^.salma(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(3)
-    await typew.edit("`Cuma Mau Bilang`")
-    sleep(3)
-    await typew.edit("`Aku Sayang Kamu`")
-    sleep(1)
-    await typew.edit("`I LOVE YOU SALMA 💞`")
+    sleep(2)
+    await typew.edit("`𝐀𝐊𝐔 𝐂𝐔𝐌𝐀 𝐌𝐀𝐔 𝐁𝐈𝐋𝐀𝐍𝐆..`")
+    sleep(2)
+    await typew.edit("`𝐀𝐊𝐔 𝐒𝐀𝐘𝐀𝐍𝐆 𝐊𝐀𝐌𝐔..𝐓𝐄𝐓𝐀𝐏 𝐉𝐀𝐃𝐈 𝐒𝐀𝐋𝐌𝐀 𝐘𝐀𝐍𝐆 𝐀𝐊𝐔 𝐊𝐄𝐍𝐀𝐋 𝐘𝐀..`")
+    sleep(2)
+    await typew.edit("`𝐈 𝐋𝐎𝐕𝐄 𝐔 𝐒𝐀𝐋𝐌𝐀 💞`")
 # Create by myself @localheart
 
 
 @register(outgoing=True, pattern='^.semangat(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(3)
+    sleep(2)
     await typew.edit("`Apapun Yang Terjadi`")
-    sleep(3)
+    sleep(2)
     await typew.edit("`Tetaplah Bernapas`")
-    sleep(1)
+    sleep(2)
     await typew.edit("`Dan Selalu Bersyukur`")
 # Create by myself @localheart
 
@@ -41,10 +41,26 @@ async def typewriter(typew):
 @register(outgoing=True, pattern='^.crips(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(3)
+    sleep(2)
     await typew.edit("`𝐒𝐀𝐋𝐀𝐌 𝐃𝐀𝐑𝐈 𝐊𝐀𝐌𝐈 𝐋𝐀 𝐑𝐄𝐆𝐈𝐎𝐍𝐄 𝐂𝐑𝐈𝐏𝐒`")
-    sleep(3)
-    await typew.edit("`𝐈𝐍𝐓𝐈𝐍𝐘𝐀 𝐋𝐎 𝐒𝐄𝐌𝐔𝐀 𝐍𝐆𝐄𝐍𝐓𝐎𝐓`")
-    sleep(1)
-    await typew.edit("`𝐂𝐔𝐌𝐀 𝐂𝐑𝐈𝐏𝐒 𝐘𝐀𝐍𝐆 𝐊𝐄𝐑𝐄𝐍 𝐆𝐀 𝐀𝐃𝐀 𝐎𝐁𝐀𝐓!!`")
+    sleep(2)
+    await typew.edit("`𝐋𝐔 𝐒𝐄𝐌𝐔𝐀 𝐍𝐆𝐄𝐍𝐓𝐎𝐓, 𝐊𝐔𝐑𝐀𝐏𝐀𝐍 𝐊𝐄𝐑𝐃𝐈𝐋, 𝐊𝐎𝐃𝐎𝐊 𝐁𝐀𝐍𝐂𝐄𝐓`")
+    sleep(2)
+    await typew.edit("`𝐊𝐄𝐍𝐀𝐏𝐀 ? 𝐊𝐄𝐒𝐄𝐋 ? 𝐊𝐄𝐒𝐄𝐋 𝐀𝐉𝐀 𝐄𝐌𝐀𝐍𝐆 𝐆𝐔𝐀 𝐏𝐈𝐊𝐈𝐑𝐈𝐍𝐀𝐍 𝐊𝐄𝐑𝐃𝐈𝐋, 𝐊𝐎𝐃𝐎𝐊 𝐁𝐀𝐍𝐂𝐄𝐓!!`")
+    sleep(2)
+    await typew.edit("`𝐏𝐀𝐃𝐀 𝐈𝐍𝐓𝐈𝐍𝐘𝐀 𝐂𝐔𝐌𝐀 𝐂𝐑𝐈𝐏𝐒 𝐘𝐀𝐍𝐆 𝐊𝐄𝐑𝐄𝐍𝐍𝐘𝐀 𝐆𝐀 𝐀𝐁𝐈𝐒-𝐀𝐁𝐈𝐒!!`")
+# Create by myself @localheart
+
+
+@register(outgoing=True, pattern='^.zz(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(2)
+    await typew.edit("`𝐍𝐈𝐇 𝐆𝐔𝐀 𝐁𝐈𝐋𝐀𝐍𝐆𝐈𝐍`")
+    sleep(2)
+    await typew.edit("`𝐂𝐄𝐖𝐄 𝐃𝐎𝐔𝐁𝐋𝐄 𝐙𝐙 𝐂𝐀𝐊𝐄𝐏 𝐒𝐄𝐌𝐔𝐀 𝐆𝐀 𝐀𝐃𝐀 𝐎𝐁𝐀𝐓`")
+    sleep(2)
+    await typew.edit("`𝐒𝐄𝐋𝐀𝐈𝐍 𝐂𝐄𝐖𝐄 𝐃𝐎𝐔𝐁𝐋𝐄 𝐙𝐙 𝐈𝐓𝐔 𝐘𝐀𝐍𝐆 𝐋𝐀𝐈𝐍 𝐊𝐄𝐍𝐓𝐀𝐍𝐆, 𝐊𝐀𝐘𝐀 𝐏𝐈𝐍𝐆𝐆𝐈𝐑𝐀𝐍 𝐊𝐀𝐎𝐒 𝐊𝐔𝐓𝐀𝐍𝐆 !!`")
+    sleep(2)
+    await typew.edit("`𝐏𝐀𝐃𝐀 𝐈𝐍𝐓𝐈𝐍𝐘𝐀 𝐂𝐔𝐌𝐀 𝐃𝐎𝐔𝐁𝐋𝐄 𝐙𝐙 𝐘𝐀𝐍𝐆 𝐀𝐃𝐀 𝐃𝐈 𝐇𝐀𝐓𝐈 !!`")
 # Create by myself @localheart
